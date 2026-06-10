416344c3bc5630038eed3320492b4da8844aafab
Camilo Torres Callejas <camilo.torresc@perficient.com>
2026-06-10 10:32:44 -0500

    comment out automapper

--- MappersComparasion.Shared/Mappers/AutoMapperProfile.cs
+++ MappersComparasion.Shared/Mappers/AutoMapperProfile.cs
@@ -1,13 +1,13 @@
-using AutoMapper;
-using MappersComparasion.Models;
-
-namespace MappersComparasion.Mappers;
-
-public class UserMappingProfile : Profile
-{
-    public UserMappingProfile()
-    {
-        CreateMap<User, UserDto>()
-            .ForMember(d => d.City, opt => opt.MapFrom(s => s.Address.City));
-    }
-}
+// using AutoMapper;
+// using MappersComparasion.Models;
+//
+// namespace MappersComparasion.Mappers;
+//
+// public class UserMappingProfile : Profile
+// {
+//     public UserMappingProfile()
+//     {
+//         CreateMap<User, UserDto>()
+//             .ForMember(d => d.City, opt => opt.MapFrom(s => s.Address.City));
+//     }
+// }
