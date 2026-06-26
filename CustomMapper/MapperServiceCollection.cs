a09e4e8b2cc628109596175437e233f1b3195d64
Camilo Torres Callejas <camilo.torresc@perficient.com>
2026-06-26 14:49:55 -0500

    Add multiple assemblies marker

--- CustomMapper/MapperServiceCollection.cs
+++ CustomMapper/MapperServiceCollection.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CustomMapper;
@@ -16,8 +17,22 @@ public static class MapperServiceCollection
     /// <returns>The updated service collection.</returns>
     public static IServiceCollection AddMappers<TAssemblyMarker>(this IServiceCollection services,
         ServiceLifetime serviceLifetime = ServiceLifetime.Scoped)
+        => services.AddMappers([typeof(TAssemblyMarker)], serviceLifetime);
+
+    /// <summary>
+    /// Registers all mapping profiles found in the assemblies of the specified marker types and the mapper service in the dependency injection container.
+    /// </summary>
+    /// <param name="services">The service collection to which the mapping profiles and mapper service will be added.</param>
+    /// <param name="assemblyMarkers">The types used to locate the assemblies containing the mapping profiles.</param>
+    /// <param name="serviceLifetime">The lifetime of the mapping profile services.</param>
+    /// <returns>The updated service collection.</returns>
+    public static IServiceCollection AddMappers(this IServiceCollection services,
+        IEnumerable<Type> assemblyMarkers,
+        ServiceLifetime serviceLifetime = ServiceLifetime.Scoped)
     {
-        foreach (var type in typeof(TAssemblyMarker).Assembly.GetTypes())
+        var assemblies = assemblyMarkers.Select(t => t.Assembly).Distinct();
+
+        foreach (var type in assemblies.SelectMany(a => a.GetTypes()))
         {
             if (type is not { IsClass: true, IsAbstract: false })
                 continue;
